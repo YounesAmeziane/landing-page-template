321fd080f19ec7a8ce33a1136543cc73fa92850c
Younes Ameziane <73488317+YounesAmeziane@users.noreply.github.com>
2021-09-12 19:42:52 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="prova.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.1.0/css/all.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="https://www.instagram.com/code_l34x"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="http://Wa.me/+212638869954"><i class="fab fa-whatsapp"></i></a></li>
+                <li><a href="https://www.l34x.ml"><i class="fas fa-globe"></i></a></li>
+                <li><a href=""></a></li>
+            </ul>
+            <div class="name">
+                <h1>ameziane younes</h1>
+                <h5>visit my site for more www.l34x.ml</h5>
+            </div>
+        </nav>
+    </header>
+    <div class="container">
+        <div class="box box-1">
+            <div class="hover">
+                <h5>powered by leax</h5>
+                <h1>Philosofy</h1>
+                <p>
+                    Lotus Cars Limited is a British automotive company headquartered in Norfolk
+
+                    Lotus was previously involved in Formula One racing, via Team Lotus, winning the Formula One World Championship seven times.
+
+                    Lotus Cars was founded and owned for many years by Colin Chapman.
+
+                    The engineering consultancy firm Lotus Engineering, an offshoot of Lotus Cars, has facilities in the United Kingdom, United States, China, and Malaysia. 
+                </p>
+            </div>
+        </div>
+        <div class="box box-2">
+            <div class="set">
+                <div class="row" id="one">
+                    <h3>Tecnology</h3>
+                </div>
+                <div class="row" id="two">
+                    <h3>Fast</h3>
+                </div>
+            </div>
+            <div class="set">
+                <div class="row" id="tre">
+                    <h3>Strongness</h3>
+                </div>
+                <div class="row" id="four">
+                    <h3>Style</h3>
+                </div>
+            </div>            
+        </div>
+    </div>
+</body>
+</html>
